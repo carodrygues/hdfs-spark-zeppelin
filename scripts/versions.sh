076a320c66f4353a921bcc5574b5929343457c87
carodrygues <73800435+carodrygues@users.noreply.github.com>
2021-06-09 01:25:17 -0300

    Add files via upload

---
+++ scripts/versions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# Define version information for the component software
+#
+# hadoop
+HADOOP_VERSION=hadoop-2.7.6
+
+# hive
+HIVE_VERSION=2.3.3
+
+# HBase
+HBASE_VERSION=1.4.8
+
+# spark
+SPARK_VERSION=spark-2.3.0
+
+# Tez
+TEZ_VERSION=0.9.1
+
+# Zeppelin 
+ZEPPELIN_VERSION=0.9.0
